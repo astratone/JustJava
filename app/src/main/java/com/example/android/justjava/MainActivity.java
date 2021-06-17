f8a1f43ec3a63e783d59827e2f21fa53339a8517
unknown <astratone@gmail.com>
2021-06-17 00:06:06 -0300

    App atualiazado

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,20 +1,10 @@
-/**
- * IMPORTANT: Make sure you are using the correct package name. 
- * This example uses the package name:
- * package com.example.android.justjava
- * If you get an error when copying this code into Android studio, update it to match teh package name found
- * in the project's AndroidManifest.xml file.
- **/
-
 package com.example.android.justjava;
-
 
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -33,50 +23,65 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
-        quantity = quantity + 1;
-        display(quantity);
+        quantity += 1;
+        displayQuantity(quantity);
     }
 
     /**
      * This method is called when the minus button is clicked.
      */
-    public void decrement(View view){
-        quantity = quantity - 1;
-        display(quantity);
+    public void decrement(View view) {
+        quantity -= 1;
+        displayQuantity(quantity);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity * 5;
-        String priceMessage = "Total: $";
-        priceMessage = priceMessage + price + "\nThank you!";
+        int price = calculatePrice();
+        String priceMessage = createOrderSummary(price);
         displayMessage(priceMessage);
 
+    }
+
+    /**
+     * Create summary of the screen
+     *
+     * @param price of the order.
+     * @return text summary.
+     */
+    private String createOrderSummary(int price) {
+        String priceMessage = "Name: Your name";
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
+    }
+
+    /**
+     * Calculates the price of the order.
+     * @return Total price.
+     */
+    private int calculatePrice() {
+        return quantity * 5;
 
     }
 
     /**
      * This method displays the given quantity value on the screen.
+     * @param number
      */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+    private void displayQuantity(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-    }
-    /**
-     * This method displays the given price value on the screen.
-     */
-    private void displayPrice(int number2) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number2));
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 }
